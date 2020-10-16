d5915d860ad241bb2947df7abca5ca9cb4caffe5
CMunozMontoya <58111973+CMunozMontoya@users.noreply.github.com>
2020-10-16 10:24:04 -0300

    Add files via upload

--- t3.py
+++ t3.py
@@ -8,16 +8,19 @@ archivo = open("codigo.html","w")
 
 #cifrar----------------
 cipher = blowfish.Cipher(b"abcd1234")
-block = bytes("Hola mundo!",'utf-8')
-#print(block)
+block = bytes(8)
+print(block)
 mensaje_cifrado = cipher.encrypt_block(block)
 #texto_plano = cipher.decrypt_block(ciphertext)
 
-texto_oculto = "<div class='blowfish' id='" + str(mensaje_cifrado) + "'></div>"
+
+texto_oculto = "<div class='blowfish' id='mensaje_cifrado'>" + str(mensaje_cifrado) + "</div>"
 
 #escribir html----------
 #TEST
+archivo.write("<!DOCTYPE html>\n<html>\n<body>\n\n")
 archivo.write("<p>Este sitio contiene un mensaje secreto</p>")
 archivo.write(texto_oculto)
+archivo.write("\n</body>\n</html>")
 
 archivo.close()

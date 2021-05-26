8d985ea90f62d8ada39dc9506501b5f148990849
CMunozMontoya <58111973+CMunozMontoya@users.noreply.github.com>
2021-05-25 23:50:36 -0400

    Update decode.js

--- decode.js
+++ decode.js
@@ -2,18 +2,14 @@ function desencriptar (){
 	
 	var key = document.querySelectorAll('#llave')[0].value;
 	var mensaje = document.querySelectorAll('.blowfish')[0].id;
-
+	var mode = "ecb"
+	
 	//console.log(key);
 	//console.log(mensaje);
 
-	var mode = "ecb"
-
 	var bf = new Blowfish(key,mode);
-
 	var encrypted = bf.base64Decode(mensaje);
 	var decrypted = bf.decrypt(encrypted);
 
-
 	document.querySelectorAll('.blowfish')[0].innerHTML="mensaje: " + decrypted;
-
 }
